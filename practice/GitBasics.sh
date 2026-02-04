d4ecf9882976dfec181ba1fa6ce566a74ac3126f
owenkanter <owen.kanter@student.montana.edu>
2026-02-04 15:01:18 -0700

    Last Commit Didn't Add Practice Folder - Added Script to Practice Folder

---
+++ practice/GitBasics.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+#Basic Git Commands
+	git add . # adds all files in the master directory
+	git commit -m "NOTE" # commits files with specified note
+	git push # pushes latest commit to github
